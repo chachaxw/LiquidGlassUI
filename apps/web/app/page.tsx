42f294047385e896fafdd4177802eb40959ca457
Chacha <chachazw@gmail.com>
2025-06-12 22:10:31 +0800

    feat: update example page

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Button } from "@workspace/ui/components/button";
 import background from "@/assets/images/background.png";
+import background1 from "@/assets/images/background1.png";
 
 export default function Page() {
   return (
@@ -19,12 +20,12 @@ export default function Page() {
       </div>
       <div className="relative flex items-center justify-center min-h-svh">
         <Image
-          src={background}
-          alt="background"
+          src={background1}
+          alt="background1"
           className="absolute w-full min-h-svh inset-0 -z-10 object-cover"
         />
       </div>
-      <Button className="w-40 fixed bottom-20 left-1/2 -translate-x-1/2">Button</Button>
+      <Button className="w-40 fixed bottom-20 left-1/2 -translate-x-1/2">Liquid Glass</Button>
     </>
   );
 }
